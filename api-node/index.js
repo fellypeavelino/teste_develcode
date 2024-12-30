4ed669cb797c01550075d3c53de0340ea27451ea
fellypeavelino <fellypeavelino@hotmail.com>
2024-12-29 21:08:20 -0300

    tentando configura o swagger

--- api-node/index.js
+++ api-node/index.js
@@ -30,6 +30,59 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
+/**
+ * @swagger
+ * /process-payment:
+ *   post:
+ *     summary: Processa um pagamento
+ *     description: Endpoint que processa pagamentos para pedidos. Requer o cabeçalho Authorization.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               orderId:
+ *                 type: integer
+ *                 description: ID único do pedido.
+ *                 example: 12345
+ *               amount:
+ *                 type: number
+ *                 description: Valor total do pagamento.
+ *                 example: 100.50
+ *     responses:
+ *       200:
+ *         description: Pagamento processado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Payment processed successfully
+ *       400:
+ *         description: Cabeçalho Authorization está ausente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Authorization header is missing
+ *       401:
+ *         description: Token de autorização inválido.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid Authorization token
+ */
 app.post('/process-payment', (req, res) => {
     const { pedidoId, valor } = req.body;
     // Captura o valor do cabeçalho Authorization
@@ -45,7 +98,7 @@ app.post('/process-payment', (req, res) => {
     if (authorizationHeader !== 'develcode') {
         return res.status(401).json({ message: 'Invalid Authorization token' });
     }
-    
+
     // Simular processamento de pagamento
     const sucesso = Math.random() > 0.5; // 50% de chance de sucesso
     console.log(sucesso,  pedidoId, valor );
